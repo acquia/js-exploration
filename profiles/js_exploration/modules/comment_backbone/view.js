f5e52c49d4ef61e623bcebac37ab10880c62adb2
Wim Leers <wim.leers@acquia.com>
2016-01-29 16:03:27 +0100

    First baby step: retrieve JSON representation of all comments on the current page and console.log() them.

--- profiles/js_exploration/modules/comment_backbone/view.js
+++ profiles/js_exploration/modules/comment_backbone/view.js
@@ -1,1 +1,40 @@
-console.log(Backbone);
+(function ($, Drupal) {
+
+  'use strict';
+
+  function getComment(commentID) {
+    jQuery.ajax({
+      url: Drupal.url('comment/' + commentID) + '?_format=json',
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      success: function(data, status, xhr) {
+        console.log(data);
+      }
+    });
+  }
+
+  Drupal.behaviors.commentBackbonePoC = {
+    attach: function (context) {
+      var $context = $(context);
+
+      // Find all comments, if any.
+      var $comments = $context.find('[data-comment-backbone]').once('contextual-render');
+      if ($comments.length === 0) {
+        return;
+      }
+
+      // Collect the IDs for all comments.
+      var ids = [];
+      $comments.each(function () {
+        ids.push($(this).attr('data-comment-backbone'));
+      });
+
+      for (var i = 0; i < ids.length; i++) {
+        getComment(ids[i]);
+      }
+    }
+  }
+
+})(jQuery, Drupal);
