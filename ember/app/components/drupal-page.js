1cb62e66c32cd7d4e1db3d52208c4d2fb4ac2e1a
Edward Faulkner <ef@alum.mit.edu>
2016-04-21 01:24:47 -0400

    drupalSettings and stylesheets

--- ember/app/components/drupal-page.js
+++ ember/app/components/drupal-page.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
-const { $, RSVP } = Ember;
+import RSVP from 'rsvp';
+import $ from 'jquery';
+/* global Drupal */
 
 export default Ember.Component.extend({
   drupalLinks: Ember.inject.service(),
@@ -27,19 +29,24 @@ export default Ember.Component.extend({
         // re-enable any overlaid content so that it can wormhole
         // itself into the server-rendered DOM.
         this.set('showingOverlay', true);
+        Drupal.attachBehaviors(elt[0], page.get('drupalSettings'));
       });
     }
   },
 
   appendPage($elt, page) {
-    return new RSVP.Promise(resolve => {
-      if (!page.prerendered) {
-        page.get('content').forEach(node => {
-          $elt[0].appendChild(node);
-        });
-      }
-      resolve();
+    if (page.prerendered) {
+      return RSVP.resolve();
+    }
+    return this.appendStyles($elt, page.get('styles')).finally(() => {
+      page.get('content').forEach(node => $elt[0].appendChild(node));
     });
+  },
+
+  appendStyles($element, styles) {
+    let stylesLoaded = styles.map(s => styleLoaded(s));
+    $element.append(styles);
+    return RSVP.allSettled(stylesLoaded);
   },
 
   click(event) {
@@ -52,3 +59,27 @@ export default Ember.Component.extend({
     }
   }
 });
+
+// <link> tags do not reliably produce load events, particularly if
+// the CSS is already cached.
+function styleLoaded(element) {
+  if (element.tagName !== 'LINK') {
+    return RSVP.resolve();
+  }
+  return new RSVP.Promise((resolve, reject) => {
+    $(element)
+      .on('load', resolve)
+      .on('error', reject);
+    let started = Date.now();
+    let interval = setInterval(() => {
+      if (Date.now() - started > 1000) {
+        clearInterval(interval);
+        reject();
+      } else if (Array.from(document.styleSheets).find(s => s.ownerNode === element)) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 20);
+
+  });
+}
--- ember/app/models/drupal-page.js
+++ ember/app/models/drupal-page.js
@@ -10,6 +10,14 @@ export default DS.Model.extend({
     return this.get('htmlParser').parse(this.get('text'));
   }),
 
+  assets: Ember.computed('document', function() {
+    return Array.from(
+      this.get('document')
+        .querySelectorAll('script[type="application/x-drupal-assets"]')
+    ).map(script => JSON.parse(script.innerHTML))
+      .reduce((a,b) => a.concat(b), []);
+  }),
+
   title: Ember.computed('document', function() {
     let titleTag = this.get('document').querySelector('title');
     if (titleTag) {
@@ -20,6 +28,31 @@ export default DS.Model.extend({
   content: Ember.computed('document', function() {
     let body = importNode(this.get('document').querySelector('body'));
     return Array.from(body.childNodes);
+  }),
+
+  styles: Ember.computed('assets', function() {
+    return this.get('assets').filter(
+      a => a['#tag'] === 'link' && a['#attributes'] && a['#attributes'].rel === 'stylesheet'
+    ).map(a => {
+      let attrs = a['#attributes'];
+      let link = document.createElement('link');
+      link.setAttribute('media', attrs.media);
+      link.setAttribute('href', attrs.href);
+      link.setAttribute('rel', attrs.rel);
+      return link;
+    });
+  }),
+
+  drupalSettings: Ember.computed('assets', function() {
+    let tag = this.get('assets').find(
+      a =>
+        a['#tag'] === 'script' &&
+        a['#attributes'] &&
+        a['#attributes']['data-drupal-selector'] === 'drupal-settings-json'
+    );
+    if (tag) {
+      return JSON.parse(tag['#value']);
+    }
   }),
 
   embeddedComponents: Ember.computed(function() {
