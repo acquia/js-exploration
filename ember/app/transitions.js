2c8aa1b04e165bc303e87905ed79cc83d6bda0c7
Edward Faulkner <ef@alum.mit.edu>
2016-04-21 11:22:33 -0400

    animation support
    Not enabled yet because the logged-in mode toolbars misbehave.

---
+++ ember/app/transitions.js
@@ -0,0 +1,13 @@
+export default function() {
+  // this.transition(
+  //   this.fromRoute('index'),
+  //   this.toRoute('drupal-rendered'),
+  //   this.use('to-left'),
+  //   this.reverse('to-right')
+  // );
+
+  // this.transition(
+  //   this.withinRoute('drupal-rendered'),
+  //   this.use('fade')
+  // );
+}
